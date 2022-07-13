68658f49ba667eda531d84e9698fcc210b3ac018
John Doe <sailalith1993@gmail.com>
2022-07-13 22:55:34 +0530

    adding a hardcoded overlay cart

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react";
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
-
+import Cart from "../Cart/Cart";
 const Meals = () => {
   return (
     <Fragment>
+      <Cart />
       <MealsSummary />
       <AvailableMeals />
     </Fragment>
